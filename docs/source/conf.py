521e7534e32947e829f892828a254599640e10a5
Peter Urban <peter.urban@ugent.be>
2022-05-23 16:55:09 +0200

    changed license of build file (to insignificant)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,6 +1,6 @@
 # SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
 #
-# SPDX-License-Identifier: MPL-2.0
+# SPDX-License-Identifier: CC0-1.0
 
 # Configuration file for the Sphinx documentation builder.
 
