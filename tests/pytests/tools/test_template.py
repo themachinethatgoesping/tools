ea434c2e73569ac204850399c8afce9af87796f3
Peter Urban <peter.urban@ugent.be>
2022-06-10 10:53:15 +0200

    python source file moving

---
+++ tests/pytests/tools/test_template.py
@@ -0,0 +1,19 @@
+# SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from themachinethatgoesping.tools import timeconv
+
+import time
+import pytest
+
+
+#define class for grouping (test sections)
+class Test_tools_template:
+    #define actual tests (must start with "test_"
+    #test case 1
+    def test_tests_should_run(self):
+        assert 1 == 1
+
+
+
