8c33bd3fce47a3755027b434881f69d2b1ec66cb
Peter Urban <peter.urban@ugent.be>
2022-06-21 23:47:35 +0200

    added quaternion conversion functions

---
+++ src/tests/rotationfunctions/quaternions.test.cpp
@@ -0,0 +1,222 @@
+// SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#include <catch2/catch.hpp>
+
+#include <boost/algorithm/algorithm.hpp>
+#include <boost/random.hpp>
+#include <chrono>
+
+#include "../../src/tools/rotationfunctions/quaternions.hpp"
+
+// using namespace testing;
+using namespace std;
+using namespace themachinethatgoesping::tools;
+
+#define TESTTAG "[rotationfunctions]"
+
+// conversions
+static const double to_rad     = M_PI / 180;
+static const double to_degrees = 1 / to_rad;
+
+TEST_CASE("normalize_angles", TESTTAG)
+{
+    SECTION("normalize_angles_rad and normalize_angles_degrees should produce the same results")
+    {
+        // random generator. Use fixed seed to ensure repeatable results
+        boost::random::mt19937 gen(1234567);
+
+        boost::random::uniform_real_distribution<double> yaw_dist(-1000., 1000);
+        boost::random::uniform_real_distribution<double> pitch_dist(-1000., 1000.);
+        boost::random::uniform_real_distribution<double> roll_dist(-1000., 1000);
+
+        for (unsigned int i; i < 10000; ++i)
+        {
+            auto yaw   = yaw_dist(gen);
+            auto pitch = pitch_dist(gen);
+            auto roll  = roll_dist(gen);
+
+            std::array<double, 3> ypr_deg = { yaw, pitch, roll };
+            std::array<double, 3> ypr_rad = { to_rad * yaw, to_rad * pitch, to_rad * roll };
+
+            auto ypr_deg_result = rotationfunctions::normalize_angles_degrees(ypr_deg);
+            auto ypr_rad_result = rotationfunctions::normalize_angles_rad(ypr_rad);
+
+            for (unsigned int i = 0; i < 3; ++i)
+                CHECK(ypr_deg_result[i] == Approx(ypr_rad_result[i] * to_degrees));
+        }
+    }
+
+    SECTION("normalize_angles should place yaw pitch and roll into defined limiets")
+    {
+        std::vector<std::pair<std::array<double, 3>, std::array<double, 3>>> YPR = {
+            {{ -720., 0., -720. },     { 0., 0., 0. }       }, // 0
+            { { -540., -90., -540. },  { 180., -90., -180. }}, // 1
+            { { -360., 80., -360. },   { 0., 80., 0. }      }, // 2
+            { { -270., -40., -270. },  { 90., -40., 90. }   }, // 3
+            { { -180., 30., -180. },   { 180., 30., -180. } }, // 4
+            { { -90., -30., -90. },    { 270., -30., -90. } }, // 5
+            { { -45., -45., -45. },    { 315., -45., -45. } }, // 6
+            { { 0., 0., 0. },          { 0., 0., 0. }       }, // 7
+            { { 45., 45., 45. },       { 45., 45., 45. }    }, // 8
+            { { 90., 80., 90. },       { 90., 80., 90. }    }, // 9
+            { { 135., -80., 135. },    { 135., -80., 135. } }, // 10
+            { { 180., 80., 180. },     { 180., 80., -180. } }, // 11
+            { { 225., -90, 225. },     { 225., -90, -135. } }, // 12
+            { { 270., 90, 270. },      { 270., 90, -90. }   }, // 13
+            { { 360., 0., 360. },      { 0., 0., 0. }       }, // 14
+  // --- section for pitch larger 90
+            { { 45., 100., 45. },      { 225., 80., -135. } }, // 15
+            { { 45., -100., 45. },     { 225., -80., -135. }}, // 16
+            { { 90., 120., 90. },      { 270., 60., -90. }  }, // 17
+            { { -45., -120., -45. },   { 135., -60., 135. } }, //, // 18
+            { { 456., 789., 456. },    { 96., 69., 96. }    }, // 19
+            { { -456., -789., -456. }, { 264., -69., -96. } }, // 20
+            { { -90., -300., -90. },   { 270., 60., -90. }  }, // 21
+            { { -90., 300., -90. },    { 270., -60., -90. } }, // 22
+            { { -90., -100., -90. },   { 90., -80., 90. }   }, // 23
+            { { -90., 100., -90. },    { 90., 80., 90. }    }  // 24
+        };
+
+        for (unsigned int c = 0; c < YPR.size(); ++c)
+        {
+            auto ypr = YPR[c].first;
+            auto e   = YPR[c].second;
+
+            auto ypr_new = rotationfunctions::normalize_angles_degrees(ypr);
+
+            for (unsigned int i = 0; i < 3; ++i)
+            {
+                INFO("value: " << c << '/' << i << " / " << ypr[i] << " / " << ypr_new[i] << " / "
+                               << e[i]);
+                CHECK(ypr_new[i] == Approx(e[i]).scale(1));
+            }
+        }
+    }
+}
+
+TEST_CASE("rotationfunctions: quaternion / ypr conversion", TESTTAG)
+{
+    SECTION("quaternion / ypr conversion should be reversible when within specified ranges")
+    {
+        // random generator. Use fixed seed to ensure repeatable results
+        boost::random::mt19937 gen(123456);
+
+        boost::random::uniform_real_distribution<double> yaw_dist(0., 360);
+        boost::random::uniform_real_distribution<double> pitch_dist(-90., 90.);
+        boost::random::uniform_real_distribution<double> roll_dist(-180., 180);
+
+        for (unsigned int i; i < 1000; ++i)
+        {
+            auto yaw   = yaw_dist(gen);
+            auto pitch = pitch_dist(gen);
+            auto roll  = roll_dist(gen);
+
+            std::array<double, 3> test = { yaw, pitch, roll };
+
+            auto q1 = rotationfunctions::quaternion_from_ypr(yaw, pitch, roll);
+            auto q2 = rotationfunctions::quaternion_from_ypr(test);
+
+            REQUIRE(rotationfunctions::ypr_from_quaternion(q1) ==
+                    rotationfunctions::ypr_from_quaternion(q2));
+        }
+    }
+
+    SECTION("quaternion / ypr conversion should place yaw pitch and roll into defined limiets")
+    {
+        std::vector<std::pair<std::array<double, 3>, std::array<double, 3>>> YPR = {
+            {{ -720., 0., -720. },      { 0., 0., 0. }       }, // 0
+            { { -540., -90., -540. },
+             { 180., -90., -180. }                           }, // 1 //edge case -90 pitch is not handled well due to
+  // quaternion conversion
+            { { -360., 80., -360. },    { 0., 80., 0. }      }, // 2
+            { { -270., -40., -270. },   { 90., -40., 90. }   }, // 3
+            { { -180., 30., -180. },    { 180., 30., -180. } }, // 4
+            { { -90., -30., -90. },     { 270., -30., -90. } }, // 5
+            { { -45., -45., -45. },     { 315., -45., -45. } }, // 6
+            { { 0., 0., 0. },           { 0., 0., 0. }       }, // 7
+            { { 45., 45., 45. },        { 45., 45., 45. }    }, // 8
+            { { 90., 80., 90. },        { 90., 80., 90. }    }, // 9
+            { { 135., -80., 135. },     { 135., -80., 135. } }, // 10
+            { { 180., 80., 180. },      { 180., 80., -180. } }, // 11
+            { { 225., -89.9999, 225. },
+             { 225., -89.9999, -135. }                       }, // 12 //edge case -90 pitch is not handled well due to
+  // quaternion conversion
+            { { 270., 89.9999, 270. },
+             { 270., 89.9999, -90. }                         }, // 13 //edge case -90 pitch is not handled well due to
+  // quaternion conversion
+            { { 360., 0., 360. },       { 0., 0., 0. }       }, // 14
+  // --- section for pitch larger 90
+            { { 45., 100., 45. },       { 225., 80., -135. } }, // 15
+            { { 45., -100., 45. },      { 225., -80., -135. }}, // 16
+            { { 90., 120., 90. },       { 270., 60., -90. }  }, // 17
+            { { -45., -120., -45. },    { 135., -60., 135. } }, // 18
+            { { 456., 789., 456. },     { 96., 69., 96. }    }, // 19
+            { { -456., -789., -456. },  { 264., -69., -96. } }, // 20
+            { { -90., -300., -90. },    { 270., 60., -90. }  }, // 21
+            { { -90., 300., -90. },     { 270., -60., -90. } }, // 22
+            { { -90., -100., -90. },    { 90., -80., 90. }   }, // 23
+            { { -90., 100., -90. },     { 90., 80., 90. }    }  // 24
+        };
+
+        for (unsigned int c = 0; c < YPR.size(); ++c)
+        {
+            auto ypr = YPR[c].first;
+            auto e   = YPR[c].second;
+
+            auto ypr_new =
+                rotationfunctions::ypr_from_quaternion(rotationfunctions::quaternion_from_ypr(ypr));
+
+            for (unsigned int i = 0; i < 3; ++i)
+            {
+                INFO("value: " << c << '/' << i << " / " << ypr[i] << " / " << ypr_new[i] << " / "
+                               << e[i]);
+                CHECK(ypr_new[i] == Approx(e[i]).scale(1));
+            }
+        }
+    }
+
+    SECTION("vectorized calls should produce the same results as single calls")
+    {
+        // random generator. Use fixed seed to ensure repeatable results
+        boost::random::mt19937 gen(1234567);
+
+        boost::random::uniform_real_distribution<double> yaw_dist(-1000., 1000);
+        boost::random::uniform_real_distribution<double> pitch_dist(-1000., 1000.);
+        boost::random::uniform_real_distribution<double> roll_dist(-1000., 1000);
+
+        std::vector<std::array<double, 3>> YPR;
+        std::vector<double>                Y, P, R;
+        for (unsigned int i; i < 10000; ++i)
+        {
+            auto yaw   = yaw_dist(gen);
+            auto pitch = pitch_dist(gen);
+            auto roll  = roll_dist(gen);
+
+            YPR.push_back({ yaw, pitch, roll });
+            Y.push_back(yaw);
+            P.push_back(pitch);
+            R.push_back(roll);
+        }
+
+        auto Q1          = rotationfunctions::quaternion_from_ypr(YPR);
+        auto Q2          = rotationfunctions::quaternion_from_ypr(Y, P, R);
+        auto YPR_result1 = rotationfunctions::ypr_from_quaternion(Q1);
+        auto YPR_result2 = rotationfunctions::ypr_from_quaternion(Q2);
+
+        for (unsigned int c = 0; c < YPR.size(); ++c)
+        {
+            auto q   = rotationfunctions::quaternion_from_ypr(YPR[c]);
+            auto ypr = rotationfunctions::ypr_from_quaternion(q);
+
+            CHECK(Q1[c] == q);
+            CHECK(Q2[c] == q);
+            for (unsigned int i = 0; i < 3; ++i)
+            {
+                CHECK(YPR_result1[c][i] == Approx(ypr[i]));
+                CHECK(YPR_result2[c][i] == Approx(ypr[i]));
+            }
+        }
+    }
+}
---
+++ src/tools/rotationfunctions/quaternions.hpp
@@ -0,0 +1,303 @@
+// SPDX-FileCopyrightText: 2022 Peter Urban, Sven Schorge, GEOMAR Helmholtz
+// Centre for Ocean Research Kiel SPDX-FileCopyrightText: 2022 Peter Urban,
+// Ghent University
+//
+// SPDX-License-Identifier: MPL-2.0
+
+/**
+ * @brief some convinience functions to create eigen quaternions from yaw pitch
+ * roll values using eigen rotations Note: Rotations are created in the
+ * following order: 1. yaw, 2. pitch, 3. roll
+ *
+ * @authors Peter Urban, Sven Schorge
+ */
+
+#pragma once
+
+#include <Eigen/Eigen>
+#include <Eigen/Geometry>
+#include <array>
+#include <boost/algorithm/string.hpp>
+#include <iostream>
+#include <tuple>
+#include <unordered_map>
+#include <vector>
+
+namespace themachinethatgoesping {
+namespace tools {
+namespace rotationfunctions {
+
+/**
+ * @brief Shift angles into specified angular range: yaw [0,2pi], pitch [-p/2°, p/2°], roll [-pi,
+ * pi]
+ *
+ * @tparam floattype double or gloat
+ * @param ypr Matrix with yaw, pitch and roll value in rad
+ * @return std::array<floattype, 3> (Matrix with yaw, pitch and roll value in rad)
+ */
+template<typename floattype>
+std::array<floattype, 3> normalize_angles_rad(std::array<floattype, 3> ypr)
+{
+    /* pitch is constrained to -90° -> 90°
+     * otherwise there is two possibilites to reach every point.
+     * because: rotate(yaw,pitch,roll) == rotate(yaw+pi,pi-pitch,roll+pi)
+     * Also note: pitch = 90 degrees leads to a gimbal lock situation
+     */
+    static const floattype M_2PI    = 2. * M_PI;
+    static const floattype M_PI_3_2 = M_PI_2 * 3.;
+
+    while (ypr[1] > M_PI_3_2)
+    {
+        ypr[1] -= M_2PI;
+    }
+    while (ypr[1] < -M_PI_2)
+    {
+        ypr[1] += 2 * M_PI;
+    }
+    while (ypr[1] > M_PI_2)
+    {
+        ypr[0] -= M_PI;
+        ypr[1] = M_PI - ypr[1];
+        ypr[2] -= M_PI;
+    }
+
+    // roll is defined within -180° -> +179.0000 or +- pi
+    while (ypr[2] < M_PI)
+        ypr[2] += M_2PI;
+    while (ypr[2] >= M_PI)
+        ypr[2] -= M_2PI;
+
+    // yaw is defined within 0 - 359.9999 ° or 0 - 2pi
+    while (ypr[0] < 0)
+        ypr[0] += M_2PI;
+    while (ypr[0] >= M_2PI)
+        ypr[0] -= M_2PI;
+
+    return ypr;
+}
+
+/**
+ * @brief Shift angles into specified angular range: yaw [0°,359.99°], pitch [-90°, 90°], roll
+ * [-180¶, 179.99°]
+ *
+ * @tparam floattype double or gloat
+ * @param ypr Matrix with yaw, pitch and roll value in °
+ * @return std::array<floattype, 3> (Matrix with yaw, pitch and roll value in °)
+ */
+template<typename floattype>
+std::array<floattype, 3> normalize_angles_degrees(std::array<floattype, 3> ypr)
+{
+    //
+
+    /* pitch is constrained to -90° -> 90°
+     * otherwise there is two possibilites to reach every point.
+     * because: rotate(yaw,pitch,roll) == rotate(yaw+pi,pi-pitch,roll+pi)
+     * Also note: pitch = 90 degrees leads to a gimbal lock situation
+     */
+    while (ypr[1] > 270.)
+    {
+        ypr[1] -= 360.;
+    }
+    while (ypr[1] < -90.)
+    {
+        ypr[1] += 360.;
+    }
+    while (ypr[1] > 90.)
+    {
+        ypr[0] -= 180.;
+        ypr[1] = 180. - ypr[1];
+        ypr[2] -= 180.;
+    }
+
+    // roll is defined within -180° -> +179.999 or +- pi
+    while (ypr[2] < 180.)
+        ypr[2] += 360.;
+    while (ypr[2] >= 180.)
+        ypr[2] -= 360.;
+
+    // yaw is defined within 0 - 359.9999 ° or 0 - 2pi
+    while (ypr[0] < 0)
+        ypr[0] += 360.;
+    while (ypr[0] >= 360.)
+        ypr[0] -= 360.;
+
+    return ypr;
+}
+
+/**
+ * @brief create an eigen quaternion by rotating yaw (z axis), pitch (y axis) and roll (x axis)
+ *
+ * @tparam floattype
+ * @param ypr array that contains yaw, pitch and roll value
+ * @param input_in_degrees if true, yaw pitch and roll input values are in ° otherwise rad
+ * @return Eigen::Quaternion<floattype>
+ */
+template<typename floattype>
+Eigen::Quaternion<floattype> quaternion_from_ypr(std::array<floattype, 3> ypr,
+                                                 bool                     input_in_degrees = true)
+{
+    static const floattype to_rad = M_PI / 180.;
+
+    // convert to rad
+    if (input_in_degrees)
+    {
+        ypr[0] *= to_rad;
+        ypr[1] *= to_rad;
+        ypr[2] *= to_rad;
+    }
+
+    // creaste quaternion by rotating arround axes first, yaw, then pitch, then roll
+    Eigen::Quaternion<floattype> q =
+        Eigen::AngleAxis<floattype>(ypr[0], Eigen::Matrix<floattype, 1, 3>::UnitZ()) *
+        Eigen::AngleAxis<floattype>(ypr[1], Eigen::Matrix<floattype, 1, 3>::UnitY()) *
+        Eigen::AngleAxis<floattype>(ypr[2], Eigen::Matrix<floattype, 1, 3>::UnitX());
+
+    // normalize quaterion
+    q.normalize();
+
+    return q;
+}
+
+/**
+ * @brief create an eigen quaternion by rotating yaw (z axis), pitch (y axis) and roll (x axis)
+ *
+ * @tparam floattype floating point value
+ * @param yaw rotation arround z axis [° or rad]
+ * @param pitch rotation arround y axis [° or rad]
+ * @param roll rotation arround x axis [° or rad]
+ * @param input_in_degrees if true, yaw pitch and roll input values are in ° otherwise rad
+ * @return Eigen::Quaternion<floattype>
+ */
+template<typename floattype>
+Eigen::Quaternion<floattype> quaternion_from_ypr(floattype yaw,
+                                                 floattype pitch,
+                                                 floattype roll,
+                                                 bool      input_in_degrees = true)
+{
+    std::array<floattype, 3> ypr = { yaw, pitch, roll };
+
+    return quaternion_from_ypr(std::move(ypr), input_in_degrees);
+}
+
+template<typename floattype>
+std::array<floattype, 3> ypr_from_quaternion(Eigen::Quaternion<floattype> q,
+                                             bool                         output_to_degrees = true)
+{
+    q.normalize();
+
+    auto                     Me  = q.toRotationMatrix().eulerAngles(2, 1, 0);
+    std::array<floattype, 3> ypr = { Me[0], Me[1], Me[2] };
+
+    /* pitch is constrained to -90° -> 89.9999°
+     * otherwise there is two possibilites to reach every point.
+     * because: rotate(yaw,pitch,roll) == rotate(yaw+pi,pi-pitch,roll+pi)
+     * Also note: pitch = 90 degrees leads to a gimbal lock situation
+     */
+    ypr = normalize_angles_rad(ypr);
+
+    static const floattype to_degrees = 180 / M_PI;
+    if (output_to_degrees)
+    {
+        ypr[0] *= to_degrees;
+        ypr[1] *= to_degrees;
+        ypr[2] *= to_degrees;
+    }
+
+    return ypr;
+}
+
+// --- vectorized calls ---
+
+template<typename floattype>
+std::vector<Eigen::Quaternion<floattype>> quaternion_from_ypr(
+    const std::vector<std::array<floattype, 3>>& YPR)
+{
+    std::vector<Eigen::Quaternion<floattype>> Q;
+    Q.reserve(YPR.size());
+    for (const auto ypr : YPR)
+    {
+        Q.push_back(quaternion_from_ypr(ypr));
+    }
+
+    return Q;
+}
+
+template<typename floattype>
+std::vector<Eigen::Quaternion<floattype>> quaternion_from_ypr(const std::vector<floattype>& yaw,
+                                                              const std::vector<floattype>& pitch,
+                                                              const std::vector<floattype>& roll)
+{
+    if (yaw.size() != pitch.size() || yaw.size() != roll.size())
+    {
+        throw std::invalid_argument("ERROR[quaternion_from_ypr]: input vectors must have the same size!");
+    }
+
+    std::vector<Eigen::Quaternion<floattype>> Q;
+    Q.reserve(yaw.size());
+    for (unsigned int i = 0; i < yaw.size(); ++i)
+    {
+        Q.push_back(quaternion_from_ypr(yaw[i], pitch[i], roll[i]));
+    }
+
+    return Q;
+
+}
+
+template<typename floattype>
+std::vector<std::array<floattype, 3>> ypr_from_quaternion(const std::vector<Eigen::Quaternion<floattype>>& Q)
+{
+    std::vector<std::array<floattype, 3>> YPR;
+    YPR.reserve(Q.size());
+    for (const auto& q : Q)
+    {
+        YPR.push_back(ypr_from_quaternion(q));
+    }
+
+    return YPR;
+}
+
+// /**
+//  * @brief getQuaterniondfromVector: creates a Quaternion that holds the passed
+//  * parameters x,y,z as a Vector
+//  * @param x: x as double or float
+//  * @param y: y as double or float
+//  * @param z: z as double or float
+//  * @param w: w as double or float with the default value 0.0
+//  * @return returns the Quaternion that holds a vector
+//  */
+// template<typename floattype>
+// Eigen::Quaternion<floattype> getQuaterniondfromVector(const floattype x,
+//                                                       const floattype y,
+//                                                       const floattype z,
+//                                                       const floattype w = 0.0)
+// {
+//     Eigen::Vector3d              vec_to_rotate(x, y, z);
+//     Eigen::Quaternion<floattype> vec_to_rotate_as_quat;
+//     vec_to_rotate_as_quat.w()   = w;
+//     vec_to_rotate_as_quat.vec() = vec_to_rotate;
+//     return vec_to_rotate_as_quat;
+// }
+
+// /**
+//  * @brief getQuaterniondfromVector: creates a Quaternion that holds the passed
+//  * parameters x,y,z as a Vector
+//  * @param xyz: x, y, z as Array<floattype, 3>
+//  * @param w: w as double or float with the default value 0.0
+//  * @return returns the Quaternion that holds a vector
+//  */
+// template<typename floattype>
+// Eigen::Quaternion<floattype> getQuaterniondfromVector(const std::array<floattype, 3>& xyz,
+//                                                       const floattype                 w = 0.0)
+// {
+//     return getQuaterniondfromVector(xyz[0], xyz[1], xyz[2], w);
+// }
+
+// template<typename floattype>
+// std::vector<floattype> get_quaternion_wxyz(const Eigen::Quaternion<floattype>& q)
+// {
+//     return { q.w(), q.x(), q.y(), q.z() };
+// }
+
+} // namespace rotationfunctions
+} // namespace tools
+} // namespace themachinethatgoesping
