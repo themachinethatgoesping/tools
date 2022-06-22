22f7b9aae10c468838d80fe06285b57d325b3505
Peter Urban <peter.urban@ugent.be>
2022-06-22 14:48:31 +0200

    implemented slerp interpolator, needs debugging, not yet tested

--- src/tests/vectorinterpolators/exceptions.test.cpp
+++ src/tests/vectorinterpolators/exceptions.test.cpp
@@ -17,48 +17,50 @@ using namespace themachinethatgoesping::tools;
 
 TEST_CASE("VectorInterpolators: should throw expected exceptiosn", TESTTAG)
 {
-    // initialize test data (correct order)
-    std::vector<double> x = { -10, -5, 0, 6, 12 };
-    std::vector<double> y = { 1, 0, 1, 0, -1 };
+    SECTION("single value interpolators")
+    { // initialize test data (correct order)
+        std::vector<double> x = { -10, -5, 0, 6, 12 };
+        std::vector<double> y = { 1, 0, 1, 0, -1 };
 
-    std::vector<std::shared_ptr<vectorinterpolators::I_Interpolator<double>>> interpolators;
-    interpolators.push_back(std::make_shared<vectorinterpolators::LinearInterpolator>(x, y));
-    interpolators.push_back(std::make_shared<vectorinterpolators::NearestInterpolator>(x, y));
-    interpolators.push_back(std::make_shared<vectorinterpolators::AkimaInterpolator>(x, y));
+        std::vector<std::shared_ptr<vectorinterpolators::I_Interpolator<double>>> interpolators;
+        interpolators.push_back(std::make_shared<vectorinterpolators::LinearInterpolator>(x, y));
+        interpolators.push_back(std::make_shared<vectorinterpolators::NearestInterpolator>(x, y));
+        interpolators.push_back(std::make_shared<vectorinterpolators::AkimaInterpolator>(x, y));
 
-    for (auto interpolator : interpolators)
-    {
+        for (auto interpolator : interpolators)
+        {
 
-        // interpolator should fail if double x elements are appended
-        REQUIRE_THROWS(interpolator->append(12, -1));
-        interpolator->set_data_XY(x, y);
-        REQUIRE_THROWS(interpolator->append(11, -1));
-        interpolator->set_data_XY(x, y);
-        interpolator->append(13, -1);
+            // interpolator should fail if double x elements are appended
+            REQUIRE_THROWS(interpolator->append(12, -1));
+            interpolator->set_data_XY(x, y);
+            REQUIRE_THROWS(interpolator->append(11, -1));
+            interpolator->set_data_XY(x, y);
+            interpolator->append(13, -1);
 
-        // same for extending lists
-        REQUIRE_THROWS(interpolator->extend({ 12, 13 }, { -1, 1 }));
-        interpolator->set_data_XY(x, y);
-        REQUIRE_THROWS(interpolator->extend({ 11, 13 }, { -1, 1 }));
-        interpolator->set_data_XY(x, y);
-        REQUIRE_THROWS(interpolator->extend({ 14, 13 }, { -1, 1 }));
-        interpolator->set_data_XY(x, y);
-        REQUIRE_THROWS(interpolator->extend({ 14, 14 }, { -1, 1 }));
-        interpolator->set_data_XY(x, y);
-        interpolator->extend({ 13, 14 }, { -1, 1 });
+            // same for extending lists
+            REQUIRE_THROWS(interpolator->extend({ 12, 13 }, { -1, 1 }));
+            interpolator->set_data_XY(x, y);
+            REQUIRE_THROWS(interpolator->extend({ 11, 13 }, { -1, 1 }));
+            interpolator->set_data_XY(x, y);
+            REQUIRE_THROWS(interpolator->extend({ 14, 13 }, { -1, 1 }));
+            interpolator->set_data_XY(x, y);
+            REQUIRE_THROWS(interpolator->extend({ 14, 14 }, { -1, 1 }));
+            interpolator->set_data_XY(x, y);
+            interpolator->extend({ 13, 14 }, { -1, 1 });
 
-        // initialize test data (wrong order)
-        std::vector<double> x_wrong_order = { -5, -10, 0, 6, 12 };
-        std::vector<double> y_wrong_order = { 0, 1, 1, 0, -1 };
+            // initialize test data (wrong order)
+            std::vector<double> x_wrong_order = { -5, -10, 0, 6, 12 };
+            std::vector<double> y_wrong_order = { 0, 1, 1, 0, -1 };
 
-        // throw because x is nort sorted
-        REQUIRE_THROWS(vectorinterpolators::AkimaInterpolator(x_wrong_order, y_wrong_order));
+            // throw because x is nort sorted
+            REQUIRE_THROWS(vectorinterpolators::AkimaInterpolator(x_wrong_order, y_wrong_order));
 
-        // initialize test data (duplicates)
-        std::vector<double> x_duplicates = { -5, -10, 0, 0, 6, 12 };
-        std::vector<double> y_duplicates = { 0, 1, 1, 0, 1, -1 };
+            // initialize test data (duplicates)
+            std::vector<double> x_duplicates = { -5, -10, 0, 0, 6, 12 };
+            std::vector<double> y_duplicates = { 0, 1, 1, 0, 1, -1 };
 
-        // interpolator should fail if there is a double x element!
-        REQUIRE_THROWS(vectorinterpolators::AkimaInterpolator(x_duplicates, y_duplicates));
+            // interpolator should fail if there is a double x element!
+            REQUIRE_THROWS(vectorinterpolators::AkimaInterpolator(x_duplicates, y_duplicates));
+        }
     }
 }
--- src/tools/vectorinterpolators/slerpinterpolator.hpp
+++ src/tools/vectorinterpolators/slerpinterpolator.hpp
@@ -29,24 +29,113 @@ class SlerpInterpolator : public I_PairInterpolator<t_quaternion>
 
   public:
     SlerpInterpolator()
-        : I_PairInterpolator<t_quaternion>(
-              {{ 0, rotationfunctions::quaternion_from_ypr<double>(0, -180, -89.99) }   ,
-              { 1, rotationfunctions::quaternion_from_ypr<double>(360, 180, 89.99) }})
+        : I_PairInterpolator<t_quaternion>({
+              {0,  rotationfunctions::quaternion_from_ypr<double>(0,   -180, -89.99)},
+              { 1, rotationfunctions::quaternion_from_ypr<double>(360, 180,  89.99) }
+    })
     {
     }
 
     SlerpInterpolator(const std::vector<std::pair<double, t_quaternion>>& XY,
-                       t_extr_mode extrapolation_mode = t_extr_mode::extrapolate)
+                      t_extr_mode extrapolation_mode = t_extr_mode::extrapolate)
         : I_PairInterpolator<t_quaternion>(XY, extrapolation_mode)
     {
     }
     SlerpInterpolator(const std::vector<double>&       X,
-                       const std::vector<t_quaternion>& Y,
-                       t_extr_mode extrapolation_mode = t_extr_mode::extrapolate)
+                      const std::vector<t_quaternion>& Y,
+                      t_extr_mode extrapolation_mode = t_extr_mode::extrapolate)
         : I_PairInterpolator<t_quaternion>(X, Y, extrapolation_mode)
     {
     }
 
+    SlerpInterpolator(const std::vector<double>& X,
+                      const std::vector<double>& yaw,
+                      const std::vector<double>& pitch,
+                      const std::vector<double>& roll,
+                      bool                       input_in_degrees   = true,
+                      t_extr_mode                extrapolation_mode = t_extr_mode::extrapolate)
+        : I_PairInterpolator<t_quaternion>(
+              X,
+              rotationfunctions::quaternion_from_ypr(yaw, pitch, roll, input_in_degrees),
+              extrapolation_mode)
+    {
+    }
+
+    SlerpInterpolator(const std::vector<double>&                X,
+                      const std::vector<std::array<double, 3>>& ypr,
+                      bool                                      input_in_degrees = true,
+                      t_extr_mode extrapolation_mode = t_extr_mode::extrapolate)
+        : I_PairInterpolator<t_quaternion>(
+              X,
+              rotationfunctions::quaternion_from_ypr(ypr, input_in_degrees),
+              extrapolation_mode)
+    {
+    }
+
+    /**
+     * @brief get the interolated yaw, pitch and roll values for given x target
+     *
+     * @param target_x find the corresponding y value for this x value
+     * @param output_in_degrees if true, yaw pitch and roll input values are in ° otherwise rad
+     * @return corresponding y value
+     */
+    std::array<double, 3> interpolate_ypr(double target_x, bool output_in_degrees = true)
+    {
+        return rotationfunctions::ypr_from_quaternion(
+            I_Interpolator<t_quaternion>::interpolate(target_x), output_in_degrees);
+    }
+
+    /**
+     * @brief get the interolated yaw, pitch and roll values for given x target (vectorized call)
+     *
+     * @param targets_x vector of x values. For each of these values find the corrspondig yaw, pitch
+     * and roll value
+     * @param output_in_degrees if true, yaw pitch and roll input values are in ° otherwise rad
+     * @return corresponding y value
+     */
+    std::vector<std::array<double, 3>> interpolate_ypr(const std::vector<double>& targetsX,
+                                                       bool output_in_degrees = true)
+    {
+        std::vector<std::array<double, 3>> y_values;
+        y_values.reserve(targetsX.size());
+        for (const auto target_x : targetsX)
+        {
+            y_values.push_back(interpolate_ypr(target_x, output_in_degrees));
+        }
+
+        return y_values;
+    }
+
+    // -----------------------
+    // append/extend functions
+    // -----------------------
+    void append(double x, double yaw, double pitch, double roll, bool input_in_degrees = true)
+    {
+        I_PairInterpolator<t_quaternion>::append(
+            x, rotationfunctions::quaternion_from_ypr(yaw, pitch, roll, input_in_degrees));
+    }
+    void append(double x, std::array<double, 3> ypr, bool input_in_degrees = true)
+    {
+        I_PairInterpolator<t_quaternion>::append(
+            x, rotationfunctions::quaternion_from_ypr(ypr, input_in_degrees));
+    }
+
+    void extend(const std::vector<double>& x,
+                const std::vector<double>& yaw,
+                const std::vector<double>& pitch,
+                const std::vector<double>& roll,
+                bool                       input_in_degrees = true)
+    {
+        I_PairInterpolator<t_quaternion>::extend(
+            x, rotationfunctions::quaternion_from_ypr(yaw, pitch, roll, input_in_degrees));
+    }
+    void extend(const std::vector<double>&                x,
+                const std::vector<std::array<double, 3>>& ypr,
+                bool                                      input_in_degrees = true)
+    {
+        I_PairInterpolator<t_quaternion>::extend(
+            x, rotationfunctions::quaternion_from_ypr(ypr, input_in_degrees));
+    }
 
     /**
      * @brief Interpolate: Slerp interpolation between two values
@@ -56,11 +145,12 @@ class SlerpInterpolator : public I_PairInterpolator<t_quaternion>
      * inbetween)
      * @return Interpolated value for target position
      */
-    t_quaternion interpolate_pair(double target_x, const t_quaternion& y1, const t_quaternion& y2) const final
+    t_quaternion interpolate_pair(double              target_x,
+                                  const t_quaternion& y1,
+                                  const t_quaternion& y2) const final
     {
         return y1.slerp(target_x, y2);
     }
-    
 };
 
 } // namespace vectorinterpolators
