95f0254426dd4d99f41669fb2f4518d8b79f1eb3
Peter Urban <peter.urban@ugent.be>
2022-07-08 10:16:14 +0200

    added missing license information

--- python/tests/vectorinterpolators/test_interpolation_slerp.py
+++ python/tests/vectorinterpolators/test_interpolation_slerp.py
@@ -1,3 +1,7 @@
+# SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+#
+# SPDX-License-Identifier: MPL-2.0
+
 from themachinethatgoesping.tools import vectorinterpolators as vip
 
 import pytest
