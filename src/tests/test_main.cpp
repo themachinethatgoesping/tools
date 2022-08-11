4bf42c76cec0477ed9f7fdaa0836184216ef6773
Peter Urban <peter.urban@ugent.be>
2022-08-11 20:58:16 +0200

    adapted copyright info

--- src/tests/test_main.cpp
+++ src/tests/test_main.cpp
@@ -1,4 +1,4 @@
-// SPDX-FileCopyrightText: 2022 GEOMAR Helmholtz Centre for Ocean Research Kiel
+// SPDX-FileCopyrightText: 2022 Peter Urban, GEOMAR Helmholtz Centre for Ocean Research Kiel
 // SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
 //
 // SPDX-License-Identifier: MPL-2.0
