1e65ca39882d408a1bcb924d5948ccf4106313a5
Peter Urban <peter.urban@ugent.be>
2022-08-17 01:16:36 +0200

    pass string by reference

--- src/themachinethatgoesping/tools/pybind11_helpers/enumhelpers.hpp
+++ src/themachinethatgoesping/tools/pybind11_helpers/enumhelpers.hpp
@@ -5,7 +5,7 @@
 // SPDX-License-Identifier: MPL-2.0
 
 /**
- * @brief some macros to easily implement comon enum capabilities for for pybind classes
+ * @brief some macros to easily implement common enum capabilities for for pybind classes
  *
  * @authors Peter Urban
  */
@@ -27,36 +27,34 @@ namespace pybind_helpers {
  * @param str string that is to be converted
  */
 #define __PYENUM_FROM_STRING__(T_ENUM)                                                             \
-    .def(                                                                                          \
-        py::init(                                                                                \
-        [](const std::string str) {                                                  \
-            auto enum_value = magic_enum::enum_cast<T_ENUM>(str);                                  \
-            if (!enum_value.has_value())                                                           \
-            {                                                                                      \
+    .def(py::init([](const std::string& str) {                                                     \
+             auto enum_value = magic_enum::enum_cast<T_ENUM>(str);                                 \
+             if (!enum_value.has_value())                                                          \
+             {                                                                                     \
                                                                                                    \
-                constexpr auto enum_values = magic_enum::enum_names<T_ENUM>();                     \
-                std::string    enum_info;                                                          \
-                for (unsigned int i = 0; i < enum_values.size(); ++i)                              \
-                {                                                                                  \
-                    if (i != 0)                                                                    \
-                        enum_info += ", ";                                                         \
+                 constexpr auto enum_values = magic_enum::enum_names<T_ENUM>();                    \
+                 std::string    enum_info;                                                         \
+                 for (unsigned int i = 0; i < enum_values.size(); ++i)                             \
+                 {                                                                                 \
+                     if (i != 0)                                                                   \
+                         enum_info += ", ";                                                        \
                                                                                                    \
-                    enum_info += "\"";                                                             \
-                    enum_info += enum_values[i];                                                   \
-                    enum_info += "\"";                                                             \
-                }                                                                                  \
+                     enum_info += "\"";                                                            \
+                     enum_info += enum_values[i];                                                  \
+                     enum_info += "\"";                                                            \
+                 }                                                                                 \
                                                                                                    \
-                pybind11::print(                                                                   \
-                    fmt::format("ERROR: unknown value option '{}'! Try: [{}]", str, enum_info));   \
+                 pybind11::print(                                                                  \
+                     fmt::format("ERROR: unknown value option '{}'! Try: [{}]", str, enum_info));  \
                                                                                                    \
-                throw std::invalid_argument(                                                       \
-                    fmt::format("ERROR: unknown value option '{}'! Try: [{}]", str, enum_info));   \
-            }                                                                                      \
+                 throw std::invalid_argument(                                                      \
+                     fmt::format("ERROR: unknown value option '{}'! Try: [{}]", str, enum_info));  \
+             }                                                                                     \
                                                                                                    \
-            return enum_value.value();                                                             \
-        }),                                                                                         \
-        "Construct this enum type from string",                                                    \
-        py::arg("str"))
+             return enum_value.value();                                                            \
+         }),                                                                                       \
+         "Construct this enum type from string",                                                   \
+         py::arg("str"))
 
 }
 }
