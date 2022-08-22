ee7187cfcfd8716dffcccee931c6937021b5d324
Peter Urban <peter.urban@ugent.be>
2022-08-22 16:56:44 +0200

    added windows time to unix time conversion (cpp only)

--- src/themachinethatgoesping/tools/timeconv.hpp
+++ src/themachinethatgoesping/tools/timeconv.hpp
@@ -1,4 +1,5 @@
 // SPDX-FileCopyrightText: 2022 Peter Urban, GEOMAR Helmholtz Centre for Ocean Research Kiel
+// SPDX-FileCopyrightText: 2022 Tim Weiß, GEOMAR Helmholtz Centre for Ocean Research Kiel
 // SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
 //
 // SPDX-License-Identifier: MPL-2.0
@@ -15,6 +16,7 @@
 /* std includes */
 #include <chrono>
 #include <string>
+#include <cmath>
 
 namespace themachinethatgoesping {
 
@@ -72,6 +74,50 @@ std::string unixtime_to_datestring(
     double             UnixTime,
     unsigned int       fractionalSecondsDigits = 0,
     const std::string& format                  = std::string("%z__%d-%m-%Y__%H:%M:%S"));
+
+
+
+/**
+ * @brief Convert a windows 32bit Filetime to a unix timestamp
+ * conversion to unixtime see: http://www.frenk.com/2009/12/convert-filetime-to-unix-timestamp/ 
+ * 
+ * WARNING: converting to unixtime and then back causes small (100 ns ) rounding error * 
+ * @param highDateTime 
+ * @param lowDateTime 
+ * @return double 
+ */
+double windows_filetime_to_unixtime(uint32_t highDateTime, uint32_t lowDateTime)
+{
+    uint64_t date = (((int64_t)highDateTime) << 32) + lowDateTime;
+    static const uint64_t adjust = 11644473600000 * 10000; ///< 100-nanoseconds = milliseconds * 10000
+
+    // removes the diff between 1970 and 1601 and convert nanoseconds to seconds
+    double unixTime = ((double)(date - adjust)) / 10000000.;
+
+    return unixTime;
+}
+
+/**
+ * @brief Convert a unix timestamp to a windows 32bit Filetime
+ * conversion to 2 x 32 bit word see: https://support.microsoft.com/en-us/help/188768/info-working-with-the-filetime-structure
+ * 
+ * @param unixTime in seconds since 1970
+ * @return std::pair<uint32_t, uint32_t> 
+ */
+std::pair<uint32_t, uint32_t> unixtime_to_windows_filetime(double unixTime)
+{
+    /* just reverting the function above */
+    static const int64_t adjust = 11644473600000. * 10000.;
+
+    unixTime *= 10000000.;
+
+    uint64_t date = (uint64_t)std::llround(unixTime) + adjust;
+
+    uint32_t lowDateTime  = (uint32_t)(date & 0xFFFFFFFF);
+    uint32_t highDateTime = (uint32_t)(date >> 32);
+
+    return std::make_pair(highDateTime, lowDateTime);
+}
 
 } // namespace timeconv
 
