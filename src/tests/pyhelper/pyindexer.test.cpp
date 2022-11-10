8615cb64d87c40ac57839aa77309d6af96bb4222
Peter Urban <peter.urban@ugent.be>
2022-11-10 10:16:54 +0100

    added pyindexer helper class for easy python style indexing and slicing

---
+++ src/tests/pyhelper/pyindexer.test.cpp
@@ -0,0 +1,149 @@
+// SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+//
+// SPDX-License-Identifier: MPL-2.0
+
+#include <catch2/catch_approx.hpp>
+#include <catch2/catch_test_macros.hpp>
+
+#include "../../themachinethatgoesping/tools/pyhelper/pyindexer.hpp"
+
+// using namespace testing;
+using namespace std;
+using namespace themachinethatgoesping::tools::pyhelpers;
+
+#define TESTTAG "[pyhelper]"
+
+// conversions
+// static const double to_rad     = M_PI / 180;
+// static const double to_degrees = 1 / to_rad;
+
+TEST_CASE("pyhelper:PyIndexer", TESTTAG)
+{
+    SECTION("Support common functions")
+    {
+        PyIndexer indexer(10, 1, 5, 2);
+
+        // copy operator
+        auto indexer_copy = indexer;
+        REQUIRE(indexer_copy == indexer);
+
+        // move operator
+        auto indexer_move = std::move(indexer_copy);
+        REQUIRE(indexer_move == indexer);
+    
+        // copy constructor
+        auto indexer_copy2(indexer);
+        REQUIRE(indexer_copy2 == indexer);
+
+        // check inequality
+        indexer.set_slice_indexing(2,5,2);
+        REQUIRE(indexer_copy2 != indexer);
+        REQUIRE(indexer_move != indexer);
+
+        // check that printing to stream does not crash
+        std::stringstream ss;
+        indexer.print(ss);
+
+        // test internal to/from binary functions
+        auto buffer = indexer.to_binary();
+        auto indexer_from_binary = PyIndexer::from_binary(buffer);
+        REQUIRE(indexer_from_binary == indexer);
+
+        // test internal to/from stream functions
+        std::stringstream ss2;
+        indexer.to_stream(ss2);
+        auto indexer_from_stream = PyIndexer::from_stream(ss2);
+        REQUIRE(indexer_from_stream == indexer);       
+    }
+
+    SECTION("Reproduce precomputed results (standard)")
+    {
+        PyIndexer indexer(100);
+
+        REQUIRE(indexer(0) == 0);
+        REQUIRE(indexer(1) == 1);
+        REQUIRE(indexer(11) == 11);
+        REQUIRE(indexer(99) == 99);
+
+        REQUIRE(indexer(-100) == 0);
+        REQUIRE(indexer(-99) == 1);
+        REQUIRE(indexer(-89) == 11);
+        REQUIRE(indexer(-1) == 99);
+
+        REQUIRE_THROWS_AS(indexer(100),std::out_of_range);
+        REQUIRE_THROWS_AS(indexer(-101),std::out_of_range);
+    }
+
+    SECTION("Reproduce precomputed results (slice, positive single step)")
+    {
+        PyIndexer indexer(100,10,90,1);
+
+        REQUIRE(indexer(0) == 10);
+        REQUIRE(indexer(1) == 11);
+        REQUIRE(indexer(11) == 21);
+        REQUIRE(indexer(79) == 89);
+
+        REQUIRE(indexer(-1) == 89);
+        REQUIRE(indexer(-10) == 80);
+        REQUIRE(indexer(-79) == 11);
+        REQUIRE(indexer(-80) == 10);
+
+        REQUIRE_THROWS_AS(indexer(80),std::out_of_range);
+        REQUIRE_THROWS_AS(indexer(-81),std::out_of_range);
+    }
+
+    SECTION("Reproduce precomputed results (slice, positive multi step)")
+    {
+        PyIndexer indexer(100,10,90,3);
+
+        CHECK(indexer(0) == 10);
+        CHECK(indexer(1) == 13);
+        CHECK(indexer(11) == 43);
+        CHECK(indexer(26) == 88);
+
+        CHECK(indexer(-1) == 89);
+        CHECK(indexer(-10) == 62);
+        CHECK(indexer(-26) == 14);
+        CHECK(indexer(-27) == 11);
+
+        REQUIRE_THROWS_AS(indexer(27),std::out_of_range);
+        REQUIRE_THROWS_AS(indexer(-28),std::out_of_range);
+    }
+
+    SECTION("Reproduce precomputed results (slice, negative single step)")
+    {
+        PyIndexer indexer(100,10,90,-1);
+
+        REQUIRE(indexer(0) == 89);
+        REQUIRE(indexer(1) == 88);
+        REQUIRE(indexer(11) == 78);
+        REQUIRE(indexer(79) == 10);
+
+        REQUIRE(indexer(-1) == 10);
+        REQUIRE(indexer(-10) == 19);
+        REQUIRE(indexer(-79) == 88);
+        REQUIRE(indexer(-80) == 89);
+
+        REQUIRE_THROWS_AS(indexer(80),std::out_of_range);
+        REQUIRE_THROWS_AS(indexer(-81),std::out_of_range);
+    }
+
+    SECTION("Reproduce precomputed results (slice, negative multi step)")
+    {
+        PyIndexer indexer(100,10,90,-3);
+
+        CHECK(indexer(0) == 89);
+        CHECK(indexer(1) == 86);
+        CHECK(indexer(11) == 56);
+        CHECK(indexer(26) == 11);
+
+        CHECK(indexer(-1) == 10);
+        CHECK(indexer(-10) == 37);
+        CHECK(indexer(-26) == 85);
+        CHECK(indexer(-27) == 88);
+
+        REQUIRE_THROWS_AS(indexer(27),std::out_of_range);
+        REQUIRE_THROWS_AS(indexer(-28),std::out_of_range);
+    }
+
+}
---
+++ src/themachinethatgoesping/tools/pyhelper/pyindexer.hpp
@@ -0,0 +1,257 @@
+// SPDX-FileCopyrightText: 2022 Peter Urban, Ghent University
+// Ghent University
+//
+// SPDX-License-Identifier: MPL-2.0
+
+/**
+ * @brief A class to compute python style indexing
+ *
+ * @authors Peter Urban
+ */
+
+#include "../classhelpers/objectprinter.hpp"
+#include "../classhelpers/stream.hpp"
+
+#pragma once
+
+namespace themachinethatgoesping {
+namespace tools {
+namespace pyhelpers {
+
+/**
+ * @brief A class to compute python style indexing
+ * Allow for negative indexing (starting from the end of the container)
+ * Allow for sliced indexing (start, stop, step)
+ * 
+ */
+class PyIndexer
+{
+    size_t _vector_size; ///< the size of the vector to be indexed
+
+    bool   _is_slice    = false; ///< is this a slice?
+    size_t _index_start = 0;    ///< the start index of the slice
+    size_t _index_end   = std::numeric_limits<long>::max(); ///< the end index of the slice
+    long   _index_step  = 1;   ///< the step size of the slice
+
+    // make this private (only allowed in factory functions)
+    PyIndexer() = default;
+
+  public:
+  /**
+   * @brief Construct a new Py Indexer object   * 
+ * Allow for negative indexing (starting from the end of the container)
+   * 
+   * @param vector_size Size of the vector to be indexed
+   */
+    PyIndexer(size_t vector_size)
+        : _vector_size(vector_size)
+    {
+    }
+
+    /**
+     * @brief Construct a new Py Indexer object
+ * Allow for negative indexing (starting from the end of the container)
+ * Allow for sliced indexing (start, stop, step)
+     * 
+     * @param vector_size Size of the vector to be indexed
+     * @param index_start Start index of the slice
+     * @param index_end End index of the slice
+     * @param index_step Step size of the slice
+     */
+    PyIndexer(size_t vector_size, long start, long end, long step = 1)
+        : _vector_size(vector_size)
+    {
+        set_slice_indexing(start, end, step);
+    }
+
+    /**
+     * @brief Setup slice indexing after construction
+     * 
+     * @param start Start index of the slice
+     * @param end End index of the slice
+     * @param step Step size of the slice
+     */
+    void set_slice_indexing(long start, long end, long step = 1)
+    {
+        if (step == 0)
+            throw(std::out_of_range("PyIndexer: step is zero!"));
+
+        if (end == std::numeric_limits<long>::max())
+            end = _vector_size;
+
+        bool ivn_max = false;
+        bool ivn_min = false;
+        if (end < 0)
+        {
+            end     = long(_vector_size) + end;
+            ivn_max = true;
+        }
+
+        if (start < 0)
+        {
+            start   = long(_vector_size) + start;
+            ivn_min = true;
+        }
+
+        if (start > end)
+        {
+            std::swap(start, end);
+            step *= -1;
+            ivn_max = ivn_min;
+        }
+
+        end = end - 1 * (!ivn_max);
+
+        if (start < 0 || start >= long(_vector_size))
+            throw(std::out_of_range("PyIndexer: start is out of bounds!"));
+        if (end < 0 || end >= long(_vector_size))
+            throw(std::out_of_range("PyIndexer: end is out of bounds!"));
+        if (start >= end)
+            throw(std::out_of_range("PyIndexer: _start >= _end!"));
+
+        _is_slice    = true;
+        _index_start = start;
+        _index_end   = end;
+        _index_step  = step;
+    }
+
+    /**
+     * @brief Deactivate slice indexing
+     * 
+     */
+    void reset_slice_indexing()
+    {
+        _is_slice    = false;
+        _index_start = 0;
+        _index_end   = std::numeric_limits<long>::max();
+        _index_step  = 1;
+    }
+
+    // ----- operators (index) -----
+    /**
+     * @brief Get the index of the element at position i
+     * 
+     * @param index (python style) index of the element (can be negative)
+     * @return size_t 
+     */
+    size_t operator()(long index)
+    {
+        if (_is_slice)
+        {
+            if (_index_step < 0)
+                index += 1;
+
+            // convert index to C++ index using _index_step, _index_start, _index_end
+            index *= _index_step;
+
+            if (index < 0)
+                index += long(_index_end) + std::abs(_index_step); //_index_end == _vector_size-1
+            else
+                index += long(_index_start);
+
+            // TODO: fix error messages
+            if (size_t(index) > _index_end)
+                throw std::out_of_range(
+                    fmt::format("Index [{}] is > max [{}]! ", index - _index_start, _index_end));
+        }
+        else
+        {
+            // convert from python index (can be negative) to C++ index
+            if (index < 0)
+                index += long(_vector_size);
+            // index = python_index < 0 ? _vector_size + python_index : python_index;
+
+            if (size_t(index) >= _vector_size)
+                throw std::out_of_range(
+                    fmt::format("Index [{}] is >= max [{}]! ", index, _vector_size));
+        }
+
+        if (index < long(_index_start))
+            throw std::out_of_range(fmt::format("Index [{}] is < min [{}]! ", index, _index_start));
+
+        return size_t(index);
+    }
+
+    //----- operators (common) -----
+    bool operator!=(const PyIndexer& rhs) const { return !(rhs == *this); }
+    bool operator==(const PyIndexer& rhs) const
+    {
+        // compare data
+        if (_vector_size != rhs._vector_size)
+            return false;
+        if (_is_slice != rhs._is_slice)
+            return false;
+        if (_index_start != rhs._index_start)
+            return false;
+        if (_index_end != rhs._index_end)
+            return false;
+        if (_index_step != rhs._index_step)
+            return false;
+
+        return true;
+    }
+
+    // ----- from/to binary -----
+    /**
+     * @brief Return a PyIndexer read from a binary stream
+     * 
+     * @param is input stream
+     * @return PyIndexer 
+     */
+    static PyIndexer from_stream(std::istream& is)
+    {
+        PyIndexer indexer;
+        is.read(reinterpret_cast<char*>(&indexer._vector_size), sizeof(PyIndexer));
+
+        return indexer;
+    }
+
+    /**
+     * @brief Write a PyIndexer to a binary stream
+     * 
+     * @param os output stream
+     */
+    void to_stream(std::ostream& os)
+    {
+        os.write(reinterpret_cast<char*>(&_vector_size), sizeof(PyIndexer));
+    }
+
+    // ----- printing -----
+    /**
+     * @brief Print function, needs __CLASSHELPERS_DEFAULT_PRINTING_FUNCTIONS__ macro
+     * See also: classhelpers/objectprinter.hpp
+     * 
+     * @param float_precision Precision of floating point numbers
+     * @return classhelpers::ObjectPrinter 
+     */
+    classhelpers::ObjectPrinter __printer__(unsigned int float_precision) const
+    {
+        classhelpers::ObjectPrinter printer("PyIndexer", float_precision);
+
+        printer.register_value("_vector_size", _vector_size);
+
+        if (!_is_slice)
+            printer.register_value("_is_slice", _is_slice);
+        else
+        {
+            printer.register_section("Slice");
+            printer.register_value("_index_start", _index_start);
+            printer.register_value("_index_end", _index_end);
+            printer.register_value("_index_step", _index_step);
+        }
+
+        return printer;
+    }
+
+
+  public:
+    // -- class helper function macros --
+    // define to_binary and from_binary functions (needs to_stream and from_stream)
+    __STREAM_DEFAULT_TOFROM_BINARY_FUNCTIONS__(PyIndexer)
+    // define info_string and print functions (needs the __printer__ function)
+    __CLASSHELPERS_DEFAULT_PRINTING_FUNCTIONS__
+};
+
+}
+}
+}
