68fb7fc8a554a45f0a68e42119faa241a2df9431
Peter Urban <peter.urban@ugent.be>
2024-04-09 21:38:52 +0200

    test 3

--- python/tests/vectorinterpolators/test_common.py
+++ python/tests/vectorinterpolators/test_common.py
@@ -154,11 +154,11 @@ class Test_tools_vectorinterpolators_all:
             # check of exception is raised on duplicates
             with pytest.raises(ValueError):
                 interpolatorType(X_duplicates, Y_duplicates)
-            continue
 
             # check of exception is raised on x nan
             with pytest.raises(ValueError):
                 interpolatorType(X_nan, Y)
+            continue
 
             # check of exception is raised on x nan
             with pytest.raises(ValueError):
