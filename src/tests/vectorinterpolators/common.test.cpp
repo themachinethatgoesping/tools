ac61ab779b2c130102edcd80ac4d6b05fd981b58
Peter Urban <peter.urban@ugent.be>
2024-07-02 16:19:49 +0200

    debug co 3

--- src/tests/vectorinterpolators/common.test.cpp
+++ src/tests/vectorinterpolators/common.test.cpp
@@ -192,6 +192,8 @@ TEST_CASE("VectorInterpolators: should throw expected exceptions", TESTTAG)
             REQUIRE(interpolator->get_data_X() == x); // strong exception guarantee
             REQUIRE(interpolator->get_data_Y() == y); // strong exception guarantee
             REQUIRE_THROWS(interpolator->append(11, -1));
+            REQUIRE(interpolator->get_data_X() == x); // strong exception guarantee
+            REQUIRE(interpolator->get_data_Y() == y); // strong exception guarantee
             continue;
             interpolator->append(13, -1);
             interpolator->set_data_XY(x, y);
