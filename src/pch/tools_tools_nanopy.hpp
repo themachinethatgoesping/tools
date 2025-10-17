824593f94eddee825da97390ce1b18dcd2132878
Peter Urban <peter.urban@ugent.be>
2025-10-17 23:13:10 +0200

    formating

--- src/pch/tools_tools_nanopy.hpp
+++ src/pch/tools_tools_nanopy.hpp
@@ -3,27 +3,27 @@
 #pragma once
 
 #if !defined(__WIN32__) && !defined(_WIN32) && !defined(__WIN64__) && !defined(_WIN64)
-    #include <nanobind/nanobind.h>
-    #include <nanobind/stl/string.h>
-    #include <nanobind/stl/chrono.h>
-    #include <nanobind/stl/optional.h>
-    #include <nanobind/stl/string_view.h>
-    #include <nanobind/stl/vector.h>
+#include <nanobind/nanobind.h>
+#include <nanobind/stl/chrono.h>
+#include <nanobind/stl/optional.h>
+#include <nanobind/stl/string.h>
+#include <nanobind/stl/string_view.h>
+#include <nanobind/stl/vector.h>
 #endif
-#include <sstream>
-#include <vector>
-#include <tuple>
-#include <string>
-#include <xxhash.hpp>
-#include <fmt/format.h>
-#include <boost/iostreams/stream.hpp>
-#include <iosfwd>
 #include <boost/iostreams/concepts.hpp>
-#include <set>
-#include <optional>
-#include <streambuf>
+#include <boost/iostreams/stream.hpp>
+#include <fmt/format.h>
+#include <iosfwd>
 #include <iostream>
-#include <string_view>
-#include <magic_enum/magic_enum.hpp>
 #include <istream>
+#include <magic_enum/magic_enum.hpp>
+#include <optional>
+#include <set>
+#include <sstream>
+#include <streambuf>
+#include <string>
+#include <string_view>
+#include <tuple>
 #include <type_traits>
+#include <vector>
+#include <xxhash.hpp>
